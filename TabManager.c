43c6183c98373c317b8fafdd1f6ec4a6795134cb
Hisham Muhammad <hisham@gobolinux.org>
2006-10-05 21:46:16 +0000

    Cleanup tab handling

--- TabManager.c
+++ TabManager.c
@@ -10,7 +10,6 @@ struct TabPageClass_ {
 
 struct TabPage_ {
    Object super;
-   char* label;
    char* name;
    Buffer* buffer;
 };
@@ -23,6 +22,8 @@ struct TabManager_ {
    int h;
    int tabOffset;
    int currentPage;
+   bool redrawBar;
+   bool bufferModified;
 };
 
 extern TabPageClass TabPageType;
@@ -36,10 +37,10 @@ TabPageClass TabPageType = {
    }
 };
 
-TabPage* TabPage_new(char* name, char* label, Buffer* buffer) {
+TabPage* TabPage_new(char* name, Buffer* buffer) {
    TabPage* this = Alloc(TabPage);
-   this->name = String_copy(name);
-   this->label = String_copy(label);
+   if (name)
+      this->name = String_copy(name);
    this->buffer = buffer;
    return this;
 }
@@ -47,7 +48,6 @@ TabPage* TabPage_new(char* name, char* label, Buffer* buffer) {
 void TabPage_delete(Object* super) {
    TabPage* this = (TabPage*) super;
    free(this->name);
-   free(this->label);
    if (this->buffer)
       Buffer_delete(this->buffer);
    free(this);
@@ -62,6 +62,8 @@ TabManager* TabManager_new(int x, int y, int w, int h, int tabOffset) {
    this->tabOffset = tabOffset;
    this->items = Vector_new(ClassAs(TabPage, Object), true, DEFAULT_SIZE);
    this->currentPage = 0;
+   this->redrawBar = true;
+   this->bufferModified = false;
    return this;
 }
 
@@ -70,29 +72,65 @@ void TabManager_delete(TabManager* this) {
    free(this);
 }
 
-void TabManager_add(TabManager* this, char* name, char* label, Buffer* buffer) {
-   Vector_add(this->items, TabPage_new(name, label, buffer));
+void TabManager_add(TabManager* this, char* name, Buffer* buffer) {
+   Vector_add(this->items, TabPage_new(name, buffer));
 }
 
-Buffer* TabManager_draw(TabManager* this) {
+void TabManager_removeCurrent(TabManager* this) {
+   Vector_remove(this->items, this->currentPage);
+   TabManager_setPage(this, this->currentPage + 1);
+}
+
+TabPage* TabManager_current(TabManager* this) {
+   return (TabPage*) Vector_get(this->items, this->currentPage);
+}
+
+static inline void TabManager_drawBar(TabManager* this) {
    int items = Vector_size(this->items);
    int current = this->currentPage;
    assert(current < items);
    int x = this->x + this->tabOffset;
+   attrset(CRT_colors[TabColor]);
+   mvhline(LINES - 1, x, ' ', COLS - x);
    for (int i = 0; i < items; i++) {
       TabPage* page = (TabPage*) Vector_get(this->items, i);
       if (i == current)
          attrset(CRT_colors[CurrentTabColor]);
       else
          attrset(CRT_colors[TabColor]);
-      mvprintw(this->y + this->h - 1, x, " [%c]%s ", (page->buffer && page->buffer->modified) ? '*' : ' ', page->label);
-      x += strlen(page->label) + 5;
+      char modified;
+      char* label = "*untitled*";
+      if (page->buffer) {
+         modified = page->buffer->modified ? '*' : ' ';
+         if (page->buffer->fileName)
+            label = page->buffer->fileName;
+      } else {
+         modified = ' ';
+         if (page->name)
+            label = page->name;
+      }
+      char* base = strrchr(label, '/');
+      if (base) label = base + 1;
+      mvprintw(this->y + this->h - 1, x, " [%c]%s ", modified, label);
+      x += strlen(label) + 5;
    }
    attrset(CRT_colors[NormalColor]);
+   this->redrawBar = false;
+}
+
+Buffer* TabManager_draw(TabManager* this) {
    TabPage* page = (TabPage*) Vector_get(this->items, this->currentPage);
-   if (!page->buffer) {
+   if (page->buffer) {
+      if (page->buffer->modified != this->bufferModified) {
+         this->redrawBar = true;
+         this->bufferModified = page->buffer->modified;
+      }
+   } else {
       page->buffer = Buffer_new(this->x, this->y, this->w, this->h-1, page->name, false);
+      this->bufferModified = false;
    }
+   if (this->redrawBar)
+      TabManager_drawBar(this);
    Buffer_draw(page->buffer);
    return page->buffer;
 }
@@ -106,6 +144,7 @@ void TabManager_resize(TabManager* this, int w, int h) {
       if (page->buffer)
          Buffer_resize(page->buffer, this->w, this->h - 1);
    }
+   this->redrawBar = true;
 }
 
 int TabManager_find(TabManager* this, char* name) {
@@ -118,11 +157,13 @@ int TabManager_find(TabManager* this, char* name) {
    return 0;
 }
 
-/* private */
-static inline void TabManager_refreshCurrent(TabManager* this) {
+inline void TabManager_refreshCurrent(TabManager* this) {
    TabPage* page = (TabPage*) Vector_get(this->items, this->currentPage);
-   if (page->buffer)
+   if (page->buffer) {
       Buffer_refresh(page->buffer);
+      this->bufferModified = page->buffer->modified;
+   }
+   this->redrawBar = true;
 }
 
 void TabManager_previousPage(TabManager* this) {
@@ -141,7 +182,18 @@ void TabManager_nextPage(TabManager* this) {
 }
 
 void TabManager_setPage(TabManager* this, int i) {
-   assert(i >= 0 && i < Vector_size(this->items));
+   assert(i >= 0);
+   if (i >= Vector_size(this->items))
+      i = 0;
    this->currentPage = i;
    TabManager_refreshCurrent(this);
+}
+
+char* TabManager_getPageName(TabManager* this, int i) {
+   TabPage* page = (TabPage*) Vector_get(this->items, i);
+   if (page->buffer) {
+      return page->buffer->fileName;
+   } else {
+      return page->name;
+   }
 }
