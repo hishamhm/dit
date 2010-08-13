5c0bd8d12ae92fb9b72a2d87c75955ddfae83ee7
Hisham Muhammad <hisham@gobolinux.org>
2010-08-13 00:39:00 +0000

    ctrl-H: switch to header

--- scripts/c.lua
+++ scripts/c.lua
@@ -4,6 +4,17 @@ local cscope = require("cscope")
 function on_ctrl(key)
    if key == "D" then
       cscope.goto_definition()
+   elseif key == "H" then
+      local name = buffer:filename()
+      if name:match("%.c$") then
+         name = name:gsub("%.c$", ".h")
+      elseif name:match("%.h$") then
+         name = name:gsub("%.h$", ".c")
+      else
+         return
+      end
+      local page = tabs:open(name)
+      tabs:setPage(page)
    end
 end
 
