e88fb32cc00c5769a5a94d433392bbbad9d50752
Hisham Muhammad <hisham@gobolinux.org>
2012-11-10 00:31:36 +0000

    improve custom parentheses highlighter!

--- scripts/haskell.lua
+++ scripts/haskell.lua
@@ -23,18 +23,42 @@ end
 function highlight_line(line, y)
    local level = 0
    local out = {}
+   local stack = {}
    for i=1, #line do
-      local key
-      if line[i] == '(' then
+      local key = " "
+      local ch = line[i]
+      if ch == '(' then
+         table.insert(stack, {'(', at=i})
          level = level + 1
          key = colors[level]
-      elseif line[i] == ')' then
-         key = colors[level]
+      elseif ch == ')' then
+         if #stack == 0 or stack[#stack][1] ~= '(' then
+            key = "*"
+            level = 1
+         else
+            table.remove(stack)
+            key = colors[level]
+         end
          level = level - 1
+         if level < 0 then
+            key = "*"
+         end
+      elseif ch == '[' then
+         table.insert(stack, {'[', at=i})
+      elseif ch == ']' then
+         if #stack == 0 or stack[#stack][1] ~= '[' then
+            key = "*"
+         else
+            table.remove(stack)
+         end
       else
          key = " "
       end
       table.insert(out, key)
+   end
+   while #stack > 0 do
+      local err = table.remove(stack)
+      out[err.at] = "*"
    end
    return table.concat(out)
 end
