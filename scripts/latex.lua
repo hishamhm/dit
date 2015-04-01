9ce493a9e927d849cec3ce7016730bfc9c2788ba
Hisham Muhammad <hisham@gobolinux.org>
2015-04-01 18:36:43 -0300

    Add LaTeX script

---
+++ scripts/latex.lua
@@ -0,0 +1,15 @@
+
+local code = require("dit.code")
+
+function on_ctrl(key)
+   if key == "_" then
+      code.comment_block("//")
+   end
+end
+
+function on_fkey(key)
+   if key == "F7" then
+      code.expand_selection()
+   end
+end
+
