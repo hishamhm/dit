21b8976d4c73fce93da93b003c6ff4ab2ad5ca9c
Hisham <hisham@gobolinux.org>
2016-05-04 18:10:50 -0300

    Add my own hacked-together config.h for editorconfig

---
+++ editorconfig/src/config.h
@@ -0,0 +1,11 @@
+#define HAVE_STRDUP
+#if _WIN32
+#define WIN32
+#else
+#define UNIX
+#endif
+#define editorconfig_VERSION_MAJOR 0
+#define editorconfig_VERSION_MINOR 11
+#define editorconfig_VERSION_SUBMINOR 5
+#define editorconfig_VERSION_SUFFIX "-development"
+
