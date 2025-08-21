60e5001e0dce24961486e8c62344b13e1c037d3b
Hisham Muhammad <hisham@gobolinux.org>
2025-08-21 18:25:45 -0300

    scripts: avoid infinite loop in config handler

--- scripts/dit/config.lua
+++ scripts/dit/config.lua
@@ -9,23 +9,23 @@ function config.add(callback, fn)
    local g = _G[callback]
    if g and g ~= main_handlers[callback] then
       global_handlers[callback] = g
+      local m = main_handlers[callback] or function(...)
+         for _, handler in ipairs(local_handlers[callback]) do
+            local ok = handler(...)
+            if ok then
+               return ok
+            end
+         end
+         if global_handlers[callback] then
+            return global_handlers[callback](...)
+         end
+         return false
+      end
+      main_handlers[callback] = m
+      _G[callback] = m
    end
    local_handlers[callback] = local_handlers[callback] or {}
    table.insert(local_handlers[callback], fn)
-   local m = main_handlers[callback] or function(...)
-      for _, handler in ipairs(local_handlers[callback]) do
-         local ok = handler(...)
-         if ok then
-            return ok
-         end
-      end
-      if global_handlers[callback] then
-         return global_handlers[callback](...)
-      end
-      return false
-   end
-   main_handlers[callback] = m
-   _G[callback] = m
 end
 
 function config.add_handlers(callback, handlers)
